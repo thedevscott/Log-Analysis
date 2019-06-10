f7365baa54461b8d80e1f8ae80f99bcb50a94949
Omar Scott <thedevscott@gmail.com>
2019-06-10 18:31:19 -0400

    Corrected 'Top 3 article' query to pull titles from articles table

--- log_analysis.py
+++ log_analysis.py
@@ -26,13 +26,15 @@ def execute_query(query):
 def get_popular_articles():
     """What are the most popular three articles of all time?"""
 
-    GET_QUERY = """SELECT INITCAP(article) AS article, views
+    GET_QUERY = """SELECT articles.title, clean_articles.views
     FROM (SELECT REPLACE(SUBSTRING(log.path, 10),'-', ' ')
     AS article, count(log.path) AS views
     FROM log WHERE log.status = '200 OK' AND log.path LIKE '%article%'
     GROUP BY log.path
     ORDER BY views DESC
-    LIMIT 3) as clean_articles;
+    LIMIT 3) as clean_articles, articles
+    WHERE articles.title ~* clean_articles.article
+    ORDER BY clean_articles.views DESC;
   """
 
     return execute_query(GET_QUERY)
@@ -85,7 +87,7 @@ if __name__ == '__main__':
     print("The top 3 articles are:")
     for article in get_popular_articles():
         print("{} -- {} views".format(article[0], article[1]))
-
+"""
     print("\nThe top authors are:")
     for top in get_popular_authors():
         print("{} -- {} views".format(top[0], top[1]))
@@ -93,3 +95,4 @@ if __name__ == '__main__':
     print("\nThe day(s) request errors greater than 1%:")
     for error_day in get_error_day():
         print("{} -- {}% errors".format(error_day[0], error_day[1]))
+"""
