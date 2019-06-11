8fec2657243bc91a6559c070bd3fba24fc797a3b
Omar Scott <thedevscott@gmail.com>
2019-06-10 23:10:51 -0400

    Corrected query for popular authors and uncommented __main__ output

--- log_analysis.py
+++ log_analysis.py
@@ -43,17 +43,11 @@ def get_popular_articles():
 def get_popular_authors():
     """Who are the most popular (top 5) article authors of all time?"""
 
-    GET_QUERY = """SELECT DISTINCT top_five.name, SUM(top_five.views)
-    AS total_views
-    FROM (SELECT authors.name as name, articles.author AS author,
-    REPLACE(SUBSTRING(log.path, 10),'-', ' ') AS article,
-    COUNT(log.path) AS views
-    FROM authors, articles, log
-    WHERE authors.id = author AND log.status = '200 OK' AND log.path
-    LIKE '%article%'
-    GROUP BY log.path, authors.name, articles.author
-    ORDER BY views DESC) AS top_five
-    GROUP BY top_five.name
+    GET_QUERY = """ SELECT authors.name, COUNT(log.path) AS total_views
+    FROM authors, log, articles
+    WHERE log.status = '200 OK' AND articles.author = authors.id
+    AND SUBSTRING(log.path, 10) = articles.slug
+    GROUP BY authors.name
     ORDER BY total_views DESC;
     """
 
@@ -87,7 +81,7 @@ if __name__ == '__main__':
     print("The top 3 articles are:")
     for article in get_popular_articles():
         print("{} -- {} views".format(article[0], article[1]))
-"""
+
     print("\nThe top authors are:")
     for top in get_popular_authors():
         print("{} -- {} views".format(top[0], top[1]))
@@ -95,4 +89,3 @@ if __name__ == '__main__':
     print("\nThe day(s) request errors greater than 1%:")
     for error_day in get_error_day():
         print("{} -- {}% errors".format(error_day[0], error_day[1]))
-"""
